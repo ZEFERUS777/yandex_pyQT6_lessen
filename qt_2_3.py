56355ffe5b856e18846ec48454834252350f67f7
ZEFERUS777 <pomagomeddobri@gmail.com>
2024-09-28 16:42:27 +0300

    did the program, great work passed

--- qt_2_3.py
+++ qt_2_3.py
@@ -22,6 +22,7 @@ class MiniCalculator(QWidget):
         self.tex1_layout.addWidget(self.number_2)
 
         self.calculate_button = QPushButton('->', self)
+        self.calculate_button.clicked.connect(self.calculate)
         self.btn_conv.addWidget(self.calculate_button)
 
         self.lbl_t2_1 = QLabel('Сумма:', self)
@@ -34,20 +35,42 @@ class MiniCalculator(QWidget):
         self.tex2_layout.addWidget(self.lbl_t2_3)
         self.tex2_layout.addWidget(self.lbl_t2_4)
 
-        self.lcd_display_1 = QLCDNumber(self)
-        self.lcd_display_2 = QLCDNumber(self)
-        self.lcd_display_3 = QLCDNumber(self)
-        self.lcd_display_4 = QLCDNumber(self)
+        self.result_sum = QLCDNumber(self)
+        self.result_sub = QLCDNumber(self)
+        self.result_mul = QLCDNumber(self)
+        self.result_div = QLCDNumber(self)
 
-        self.lcd_display_layout.addWidget(self.lcd_display_1)
-        self.lcd_display_layout.addWidget(self.lcd_display_2)
-        self.lcd_display_layout.addWidget(self.lcd_display_3)
-        self.lcd_display_layout.addWidget(self.lcd_display_4)
+        self.lcd_display_layout.addWidget(self.result_sum)
+        self.lcd_display_layout.addWidget(self.result_sub)
+        self.lcd_display_layout.addWidget(self.result_mul)
+        self.lcd_display_layout.addWidget(self.result_div)
 
         self.main_layout.addLayout(self.tex1_layout)
         self.main_layout.addLayout(self.btn_conv)
         self.main_layout.addLayout(self.tex2_layout)
         self.main_layout.addLayout(self.lcd_display_layout)
+
+    def calculate(self):
+        try:
+            num1 = int(self.number_1.text())
+            num2 = int(self.number_2.text())
+
+            # Вычисляем результаты
+            self.result_sum.display(num1 + num2)
+            self.result_sub.display(num1 - num2)
+            self.result_mul.display(num1 * num2)
+
+            # Обрабатываем деление на 0
+            if num2 == 0:
+                self.result_div.display("Error")
+            else:
+                self.result_div.display(f"{num1 / num2:.3f}")
+
+        except ValueError:
+            self.result_sum.display("Error")
+            self.result_sub.display("Error")
+            self.result_mul.display("Error")
+            self.result_div.display("Error")
 
 
 if __name__ == '__main__':
